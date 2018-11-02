9b4916cd410a37a513e5b449515dea6c0158cf8a
georginajcruz <38511278+georginajcruz@users.noreply.github.com>
2018-11-02 14:41:06 -0700

    Add files via upload

--- LinkedList.java
+++ LinkedList.java
@@ -1,28 +1,21 @@
 public class LinkedList implements List{
 	public Node head; 
 	public int size; 
-	public LinkedList list;
 
 	public LinkedList(){
 		size=0;
-    list = new LinkedList();
-		
+    head = null;
 	}
 
 	public void add(Object obj) {
         Node newNode = new Node(obj);
+        newNode.next = head;
+        head = newNode;
+        size++;
 
-        if (size==0) {
-            head=newNode;
-            size++;
-            return;
-        }
-        Node node = get(size-1);
-        node.setNext(newNode);
+  }
 
-    }
-
-    public void add(int pos, Object obj){
+  public void add(int pos, Object obj){
     	if(pos<0||pos>size){
     		throw new IndexOutOfBoundsException("Index is out of the limits of the list");
     	}
@@ -31,54 +24,59 @@ public class LinkedList implements List{
     		newNode.next=head;
     		head = newNode;
     		size++;
-    	}
-    	Node previous = get(pos-1);
-    	Node newNode = new Node(obj);
-    	newNode.next = previous.next;
-    	previous.next = newNode;
-    }
-
-   	public Node get(int pos){
-      if(pos<0||pos>size){
-          throw new IndexOutOfBoundsException("Index is not in the list");
-      }
-      
-   		Node current = head;
-   		for(int i=0; i<pos; i++){
-        current=current.next;
-      }
-      return current;
-
-
-
-   	}
-
-   	public Object remove(int pos){
-   		if(pos<0||pos>size){
-    		throw new IndexOutOfBoundsException("Index is out of the limits of the list");
-    	}
-    	if(pos==0){
-    		Node current = head;
-    		head = head.next;
-    		size--;
-    		return current.getValue();
     	} else {
-    		
-    		Node previous = get(pos-1);
-    		Node current = get(pos);
-        Node temp = current;
-        current = current.next;
-    		previous.next = current;
-    		size--;
-    		return temp;
-    	}
 
-   	}
+      	Node previous = getNode(pos-1);
+      	Node newNode = new Node(obj);
+      	newNode.next = previous.next;
+      	previous.next = newNode;
+        size++;
+      }
+  }
 
-    public int size(){
-      return size;
+  public Node getNode(int pos){
+    if(pos<0||pos>size){
+      throw new IndexOutOfBoundsException("Index is not in the list");
     }
+      
+   	Node current = head;
+		for(int i=0; i<pos; i++){
+      current=current.next;
+    }
+    return current;
 
+  }
+
+  public Object get(int pos){
+    Node node = getNode(pos);
+    return node.getValue();
+  }
+
+  public Object remove(int pos){
+ 		if(pos<0||pos>size){
+    	throw new IndexOutOfBoundsException("Index is out of the limits of the list");
+    }
+  	if(pos==0){
+  		Node current = head;
+    	head = head.next;
+    	size--;
+  		return current.getValue();
+  	} else {
+    		
+    	Node previous = getNode(pos-1);
+    	Node current = getNode(pos);
+      Node temp = current;
+      current = current.next;
+  		previous.next = current;
+    	size--;
+    	return temp.getValue();
+  	}
+
+ 	}
+
+  public int size(){
+    return size;
+  }
 
 }
 
